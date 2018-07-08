00cba7361905ffee76f75a774303323038998a31
Emily Moberg <emoberg122@gmail.com>
2018-07-08 17:51:47 -0400

    debugged an indexing error in simulate_population's call to deterministic_movement; fish_data_generation now does simulate data

--- fish_data_generation.py
+++ fish_data_generation.py
@@ -1,6 +1,8 @@
 # this script simulates stage-structured population sizes in space and time for given parameters and forcin
 # functions
 
+# NB for EAM: this runs on Mushu in pipenv, so call pipenv run python fish_data_generation.py
+
 # import dependencies
 import numpy as np
 import random as random
@@ -67,7 +69,8 @@ def simulate_population(N0, PARAMS, T_FINAL, LANDSCAPE_LEN):
 			N_B[t+1][i] = (np.random.poisson(nextNB))
 			N_J[t+1][i] = (np.random.poisson(nextNJ))
 			N_A[t+1][i] = (np.random.poisson(nextNA))
-		N_A[t+1][0], N_A[t+1][1] = deterministic_movement(N_A[t+1][0], N_A[t+1][1], PARAMS) # does not generalize 
+			#print(N_B[t+1][i], N_J[t+1][i], N_A[t+1][i])
+		N_A[t+1][i], N_A[t+1][i] = deterministic_movement(N_A[t+1][i], N_A[t+1][i], PARAMS) # does not generalize 
 
 	return N_B, N_J, N_A
 
@@ -78,23 +81,14 @@ LANDSCAPE_LEN = 1
 N0 = 5
 
 # Simulates population
+
 N_B, N_J, N_A = simulate_population(N0, PARAMS, T_FINAL, LANDSCAPE_LEN)
 
+print(N_B)
+print(N_J)
+print(N_A)
+# Generates summary statistics
+
 # Makes a model to fit the data
-basic_model = pm.Model()
 
-with basic_model:
-	# priors
-	alpha = pm.Normal('alpha', mu=0, sd=10)
-
-	# Likelihodd
-	prPOPs = 1
-	for t in range(0, T_FINAL):
-		for i in range(0, LANDSCAPE_LEN):
-			PARAMS["alpha"] = growth_rate(temperatures[i], PARAMS["T0"], PARAMS["alpha0"], PARAMS["width"])
-			#print(PARAMS["alpha"])
-			nextNB, nextNJ, nextNA = deterministic_pop_dynamics(N_B[t][i], N_J[t][i], N_A[t][i], PARAMS)
-			prNB = Poisson('prNB', mu=nextNB)
-			prNJ = Poisson('prNJ', mu=nextNJ)
-			prNA = Poisson('prNA', mu=nextNA)
 
